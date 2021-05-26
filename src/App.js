5b7c1db35dadc375685bb1cf8845cbd742f5a62a
Paul Cline <mathpianop@gmail.com>
2021-05-26 14:56:38 -0500

    Sketched bare bones example using vanilla JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import alto from "./tracks/sherburne-alto.m4a"
+import tenor from "./tracks/sherburne-tenor.m4a"
 
 function App() {
+  const altoAudio = new Audio(alto);
+  const tenorAudio = new Audio(tenor);
+  
+  const playTracks = function() {   
+    tenorAudio.play();
+    altoAudio.play();
+  }
+
+  const emphasizeTenor = function() {
+    console.log("Tenor!");
+    altoAudio.volume = .2;
+  }
+
   return (
     <div className="App">
-      
+  
+      <button onClick={playTracks}>Play</button>
+      <button className="S">S</button>
+      <button className="A">A</button>
+      <button className="T" onClick={emphasizeTenor}>T</button>
+      <button className="B">B</button>
     </div>
   );
 }

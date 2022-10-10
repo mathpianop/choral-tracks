89d50a0f5331dd0636a44797704cf77aefb3f8a3
Paul <mathpianop@gmail.com>
2022-10-10 15:12:02 -0500

    Fix SongBtn onClick bug

--- src/components/SongBtn.js
+++ src/components/SongBtn.js
@@ -3,7 +3,7 @@ import "../style/SongBtn.css";
 function SongBtn(props) {
 
   return (
-    <button className="SongBtn" onClick={props.setSelectedSong(props.id)}>
+    <button className="SongBtn" onClick={() => props.setSelectedSong(props.id)}>
       {props.title}
     </button>
   )

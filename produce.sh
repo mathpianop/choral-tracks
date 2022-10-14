f90151a9f5d352b6e0e499748a009706b59f8d6e
Paul <mathpianop@gmail.com>
2022-10-13 21:32:02 -0500

    Change apiUrl to railway

--- produce.sh
+++ produce.sh
@@ -1,3 +1,3 @@
 sed -i "" "s|http://localhost:3001|https://mathpianop.github.io/choral-tracks-react|" package.json
 sed -i "" "s|http://localhost:3001|https://mathpianop.github.io/choral-tracks-react|" .env
-sed -i "" "s|http://localhost:3000/api|https://choral-tracks.herokuapp.com/api|" src/apiUrl.js
+sed -i "" "s|http://localhost:3000/api|https://choral-tracks-rails.up.railway.app/api|" src/apiUrl.js
